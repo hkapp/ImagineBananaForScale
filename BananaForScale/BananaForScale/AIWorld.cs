93e7a1f32896945ae0aae5c585a17b127b92b642
Hugo Kapp <hugo.kapp@epfl.ch>
2016-03-12 17:32:37 +0100

    Started the MonoDev project for the AI (to be fused with the simulation part later)

---
+++ BananaForScale/BananaForScale/AIWorld.cs
@@ -0,0 +1,58 @@
+﻿using System;
+
+namespace BananaForScale
+{
+	public class AIWorld
+	{
+
+		private int[,] tempMap;
+		private readonly int width;
+		private readonly int height;
+
+		public AIWorld (int width, int height)
+		{
+			tempMap = new int[width,height];
+			this.width = width;
+			this.height = height;
+		}
+
+		public int[,] GetNeighboringRegion(int x, int y, int offset)
+		{
+			int retSize = 2 * offset + 1;
+			int[,] neighs = new int[retSize,retSize];
+
+			for (int i = 0; i <= offset; i++) {
+				for (int j = 0; j <= offset; j++) {
+					int xget = x + i - offset;
+					int yget = y + j - offset;
+					neighs [i,j] = tempMap [xget,yget];
+				}
+			}
+
+			return neighs;
+		}
+
+		public void AddMesure(int x, int y, int temp)
+		{
+			if (ContainsPos (x, y)) {
+				tempMap [x, y] = temp;
+			}
+		}
+
+		public int GetAt(int x, int y)
+		{
+			if (ContainsPos(x,y)) {
+				return tempMap [x, y];
+			} else {
+				return 0;
+			}
+		}
+
+		public bool ContainsPos(int x, int y) 
+		{
+			return (x >= 0 || x < width || y >= 0 || y < height);
+		}
+
+	}
+}
+
---
+++ BananaForScale/BananaForScale/RobotAI.cs
@@ -0,0 +1,112 @@
+﻿using System;
+
+namespace BananaForScale
+{
+	public class RobotAI
+	{
+		public enum Direction {
+			UP, DOWN, LEFT, RIGHT, NONE
+		}
+
+		private const int MAP_SIZE = 100;
+		private const int NO_TEMP = 0;
+
+		private int xPos;
+		private int yPos;
+		private AIWorld world;
+
+		public RobotAI ()
+		{
+			world = new AIWorld(MAP_SIZE, MAP_SIZE);
+			xPos = MAP_SIZE / 2;
+			yPos = MAP_SIZE / 2;
+		}
+
+		public Direction MakeMove(int currentTemp) 
+		{
+			world.AddMesure (xPos, yPos, currentTemp);
+			//const int offset = 1;
+			//int[][] neighbours = world.GetNeighboringRegion (xPos, yPos, offset);
+
+			// compute target next position
+			int xTotMove = 0;
+			int yTotMove = 0;
+
+			for (int i = -1; i <= 1; i++) {
+				for (int j = -1; j <= 1; j++) {
+					int t = world.GetAt (xPos + i, yPos + j);
+
+					if (t != NO_TEMP) {
+						int dt = currentTemp - t;
+
+						if (dt < 0) {
+							// want to go in that direction
+							xTotMove += i;
+							yTotMove += j;
+						}
+						else {
+							// want to avoid that direction
+							xTotMove -= i;
+							yTotMove -= j;
+						}
+
+					}
+				}
+			}
+
+			Direction dir = DirectionForMove (xPos, yPos, xTotMove, yTotMove);
+			MoveInDirection (dir);
+			return dir;
+		}
+
+		private Direction DirectionForMove(int xOld, int yOld, int xNew, int yNew) 
+		{
+			int dx = xOld - xNew;
+			Direction xDir;
+
+			if (dx == 0) {
+				xDir = Direction.NONE;
+			} else if (dx < 0) {
+				xDir = Direction.UP;
+			} else {
+				xDir = Direction.DOWN;
+			}
+
+			int dy = yOld - yNew;
+			Direction yDir;
+
+			if (dy == 0) {
+				yDir = Direction.NONE;
+			} else if (dy < 0) {
+				yDir = Direction.LEFT;
+			} else {
+				yDir = Direction.RIGHT;
+			}
+
+			if (Math.Abs (dx) > Math.Abs (dy))
+				return xDir;
+			else
+				return yDir;
+		}
+
+		private void MoveInDirection(Direction d)
+		{
+			switch (d)
+			{
+			case Direction.UP:
+				xPos -= 1;
+				break;
+			case Direction.DOWN:
+				xPos += 1;
+				break;
+			case Direction.LEFT:
+				yPos -= 1;
+				break;
+			case Direction.RIGHT:
+				yPos += 1;
+				break;
+			}
+		}
+	}
+}
+
