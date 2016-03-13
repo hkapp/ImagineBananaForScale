66617e4286fc745a720f14de489e0ab664811f12
Hugo Kapp <hugo.kapp@epfl.ch>
2016-03-13 03:48:28 +0100

    Added MapGenerator

---
+++ BananaForScale/BananaForScale/MapGenerator.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace BananaForScale
+{
+	public struct HotSpot {
+		public int x;
+		public int y;
+		public double T;
+	}
+
+	public class MapGenerator
+	{
+		private const double avgTmp = 15;
+
+		public static double[,] Generate (int width, int height, HotSpot spot)
+		{
+			double[,] map = new double[width, height];
+			for (int x = 0; x < width; x++) {
+				for (int y = 0; y < height; y++) {
+					double dist = Math.Sqrt (Math.Pow (x - spot.x, 2) + Math.Pow (y - spot.y, 2));
+					double dt = (spot.T - avgTmp) / dist;
+					map [x, y] = avgTmp + dt;
+				}
+			}
+			return map;
+		}
+	}
+}
+
